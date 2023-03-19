547eaafe2968378e6f385e5ebfa7f860cd480550
Wonjin-Lee <claude17@naver.com>
2023-03-19 23:30:40 +0900

    Upload spring integration project

---
+++ src/main/java/io/wonjin/springintegrationstudy/SpringIntegrationStudyApplication.java
@@ -0,0 +1,13 @@
+package io.wonjin.springintegrationstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wonjin/springintegrationstudy/SpringIntegrationStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wonjin.springintegrationstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
